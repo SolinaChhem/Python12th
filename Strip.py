fede48a10971d0b13cda075b15de5c4a70fff589
Solina <158811171+SolinaChhem@users.noreply.github.com>
2024-02-05 08:24:13 +0700

    Add files via upload

---
+++ Strip.py
@@ -0,0 +1,14 @@
+my_str = "Hello, World!"
+print(my_str)
+
+# strip is for delete the white space in the beginning and the end.
+#print(my_str.strip())
+
+# Replace any word with other word
+print(my_str.replace("Hello" , "Bye Bye"))
+
+#split from a certain character
+print(my_str.split(" "))
+
+
+
